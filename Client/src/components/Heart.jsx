e07e780954797b7f647b2f7dc853043099fcd0f2
Marwa Haron <115185549+marwaharon27@users.noreply.github.com>
2024-09-25 20:20:54 +0300

    Create Heart.jsx

---
+++ Client/src/components/Heart.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { AiFillHeart } from "react-icons/ai";
+import { toggleFavorite } from '../../api/Details'; 
+
+const Heart = ({ id, isFavourite, onToggle }) => {
+  const handleToggleFavorite = async (e) => {
+    e.stopPropagation(); 
+
+    try {
+    
+      await toggleFavorite(id);
+      onToggle(); 
+    } catch (error) {
+      console.error('Error toggling favorite:', error);
+    }
+  };
+
+  return (
+    <AiFillHeart
+      size={24}
+      color={isFavourite ? "#fa3e5f" : "white"} 
+      onClick={handleToggleFavorite}
+      style={{ cursor: 'pointer' }} 
+    />
+  );
+};
+
+export default Heart;
+
