b9d0375f399cf42c7bbe093165e8a25b938efb2b
Marwa Haron <115185549+marwaharon27@users.noreply.github.com>
2024-09-28 05:05:53 +0300

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,20 @@
 import { Suspense } from 'react';
 import './App.css';
+import { MantineProvider } from "@mantine/core";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Website from './components/Pages/Website';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Properties from './components/Pages/Properties/Properties';
 import Details from './components/Details';
-
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import Bookings from "./components/Pages/Bookings/Bookings";
+import Favorites from "./components/Pages/Favourites/Favourites";
 function App() {
   return (
-    <div className="app-container">
+    <MantineProvider withGlobalStyles withNormalizeCSS>
       <BrowserRouter>
         <Suspense fallback={<div>Loading.....</div>}>
           <Routes>
@@ -16,11 +22,13 @@ function App() {
               <Route path="/" element={<Website />} />
               <Route path="/properties" element={<Properties />} />
               <Route path="/properties/:id" element={<Details />} />
+              <Route path="/bookings" element={<Bookings />} />
+							<Route path="/Favourites" element={<Favorites />} />
             </Route>
           </Routes>
         </Suspense>
       </BrowserRouter>
-    </div>
+      </MantineProvider>
   );
 }
 
