ab6c7b7c09f069225377cf2399cdea18c4e32188
Marwa Haron <115185549+marwaharon27@users.noreply.github.com>
2024-09-28 04:55:48 +0300

    Update userController.js

--- Server/controller/userController.js
+++ Server/controller/userController.js
@@ -1,6 +1,5 @@
-
 import User from "../model/User.js";
-
+import Residency from "../model/Residency.js";
 export const createUser = async (req, res) => {
 	try {
 		const { email } = req.body;
@@ -38,7 +37,18 @@ export const bookVisit = async (req, res) => {
 				.json({ message: "This residency is already booked by you" });
 		}
 
-		user.bookedVisits.push({ id, date });
+		const residency = await Residency.findById(id);
+		if (!residency) {
+			return res.status(404).json({ message: "Residency not found" });
+		}
+
+		user.bookedVisits.push({
+			id,
+			date,
+			title: residency.title,
+			price: residency.price,
+			image: residency.image,
+		});
 		await user.save();
 		res.send("Your visit is booked successfully");
 	} catch (error) {
@@ -86,20 +96,33 @@ export const cancelBooking = async (req, res) => {
 };
 
 export const toFav = async (req, res) => {
-	const { email } = req.body;
 	const { rid } = req.params;
 
 	try {
-		const user = await User.findOne({ email });
+		const defaultUserId = "66f5bf9442f6195fe2aabe56";
+		const user = await User.findById(defaultUserId);
+
 		if (!user) {
 			return res.status(404).json({ message: "User not found" });
 		}
 
-		const isFav = user.favResidenciesID.includes(rid);
+		const residency = await Residency.findById(rid);
+		if (!residency) {
+			return res.status(404).json({ message: "Residency not found" });
+		}
+
+		const isFav = user.favResidencies.some((fav) => fav.id.toString() === rid);
 		if (isFav) {
-			user.favResidenciesID.pull(rid);
+			user.favResidencies = user.favResidencies.filter(
+				(fav) => fav.id.toString() !== rid
+			);
 		} else {
-			user.favResidenciesID.push(rid);
+			user.favResidencies.push({
+				id: residency._id,
+				title: residency.title,
+				price: residency.price,
+				image: residency.image,
+			});
 		}
 
 		await user.save();
@@ -116,12 +139,38 @@ export const getAllFavorites = async (req, res) => {
 	const { email } = req.body;
 
 	try {
-		const user = await User.findOne({ email }).select("favResidenciesID");
+		const user = await User.findOne({ email }).select("favResidencies");
 		if (!user) {
 			return res.status(404).json({ message: "User not found" });
 		}
-		res.status(200).json(user.favResidenciesID);
+		res.status(200).json(user.favResidencies);
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
 };
+
+export const removeFav = async (req, res) => {
+	const { email } = req.body;
+	const { rid } = req.params;
+
+	try {
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
+
+		const favoriteIndex = user.favResidencies.findIndex(
+			(fav) => fav.id.toString() === rid
+		);
+		if (favoriteIndex === -1) {
+			return res.status(404).json({ message: "Favorite not found" });
+		}
+
+		user.favResidencies.splice(favoriteIndex, 1);
+		await user.save();
+		res.status(200).json({ message: "Favorite removed successfully" });
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
+
