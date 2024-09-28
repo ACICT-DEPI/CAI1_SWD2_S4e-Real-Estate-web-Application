d8bac3c0515bb351a4aea0d5b3f8153dceb39a26
Marwa Haron <115185549+marwaharon27@users.noreply.github.com>
2024-09-28 04:56:58 +0300

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -3,26 +3,44 @@ import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-import route from "./routes/ResidenceRoute.js"
+import route from "./routes/ResidenceRoute.js";
+import userRoutes from "./routes/userRoute.js";
 
-const app=express();
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
+
+const app = express();
+
+
 dotenv.config();
-const PORT=process.env.PORT;
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+
+const PORT = process.env.PORT || 8000; 
 const DB_URL = process.env.DB_URL;
 
 mongoose
-  .connect(DB_URL)
-  .then(() => {
-    console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-      console.log(`server is running on Port: ${PORT}`);
-    });
-  })
-  .catch(error => {
-    console.log("Error connecting to MongoDB");
-  });
+	.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("Connected to MongoDB");
 
-  app.use("/api", route);
+
+		app.listen(PORT, () => {
+			console.log(`Server is running on Port: ${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.error("Error connecting to MongoDB:", error.message);
+	});
+
+
+app.use("/api", route);
+app.use("/api/users", userRoutes); 
+
+
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).send("Something went wrong!");
+});
