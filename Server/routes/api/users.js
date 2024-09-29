aef20e2f8eb739485789931e2cffe095494d1876
Marwa Haron <115185549+marwaharon27@users.noreply.github.com>
2024-09-30 00:24:44 +0300

    Update users.js

--- Server/routes/api/users.js
+++ Server/routes/api/users.js
@@ -4,22 +4,12 @@ import {
 	getAllUsers,
 	deleteUser,
 	getUser,
-	bookVisit,
-	cancelBooking,
-	getAllBookings,
-	toFav,
-	getAllFavorites,
-	removeFav,
+
 } from "../../controller/usersController.js";
 import ROLES_LIST from "../../config/roles_list.js";
 import verifyRoles from "../../middleware/verifyRoles.js";
 
-router.post("/bookVisit/:id", bookVisit);
-router.post("/allBookings", getAllBookings);
-router.delete("/removeBooking/:id", cancelBooking);
-router.post("/toFav/:rid", toFav);
-router.post("/allFav", getAllFavorites);
-router.delete("/removeFav/:rid", removeFav);
+
 router
 	.route("/")
 	.get(verifyRoles(ROLES_LIST.Admin), getAllUsers)
