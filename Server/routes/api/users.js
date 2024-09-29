418c7e50eb7f882e348645225399f08106ae5116
Marwa Haron <115185549+marwaharon27@users.noreply.github.com>
2024-09-29 13:32:23 +0300

    Update users.js

--- Server/routes/api/users.js
+++ Server/routes/api/users.js
@@ -1,17 +1,30 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import { getAllUsers,
-    deleteUser,
-    getUser} from '../../controller/usersController.js';
-import ROLES_LIST from '../../config/roles_list.js';
-import verifyRoles from '../../middleware/verifyRoles.js';
+import {
+	getAllUsers,
+	deleteUser,
+	getUser,
+	bookVisit,
+	cancelBooking,
+	getAllBookings,
+	toFav,
+	getAllFavorites,
+	removeFav,
+} from "../../controller/usersController.js";
+import ROLES_LIST from "../../config/roles_list.js";
+import verifyRoles from "../../middleware/verifyRoles.js";
 
-router.route('/')
-    .get(verifyRoles(ROLES_LIST.Admin), getAllUsers)
-    .delete(verifyRoles(ROLES_LIST.Admin), deleteUser);
+router.post("/bookVisit/:id", bookVisit);
+router.post("/allBookings", getAllBookings);
+router.delete("/removeBooking/:id", cancelBooking);
+router.post("/toFav/:rid", toFav);
+router.post("/allFav", getAllFavorites);
+router.delete("/removeFav/:rid", removeFav);
+router
+	.route("/")
+	.get(verifyRoles(ROLES_LIST.Admin), getAllUsers)
+	.delete(verifyRoles(ROLES_LIST.Admin), deleteUser);
 
-router.route('/:id')
-    .get(verifyRoles(ROLES_LIST.Admin), getUser);
+router.route("/:id").get(verifyRoles(ROLES_LIST.Admin), getUser);
 
-
-    export default router;
+export default router;
