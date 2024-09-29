18a17a40c53cc7e4cd1e5f594bf8f03bde146b36
Marwa Haron <115185549+marwaharon27@users.noreply.github.com>
2024-09-29 15:55:15 +0300

    Update userRoute.js

--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -1,22 +1,22 @@
 // import express from "express";
-// import {
-// 	bookVisit,
-// 	cancelBooking,
-// 	createUser,
-// 	getAllBookings,
-// 	toFav,
-// 	getAllFavorites,
-// 	removeFav,
-// } from "../controller/userController.js";
+import express from "express";
+import {
+	bookVisit,
+	cancelBooking,
+	getAllBookings,
+	toFav,
+	getAllFavorites,
+	removeFav,
+} from "../controller/usersController.js";
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.post("/register", createUser);
-// router.post("/bookVisit/:id", bookVisit);
-// router.post("/allBookings", getAllBookings);
-// router.delete("/removeBooking/:id", cancelBooking);
-// router.post("/toFav/:rid", toFav);
-// router.post("/allFav", getAllFavorites); 
-// router.delete("/removeFav/:rid", removeFav); 
 
-// export default router;
+router.post("/bookVisit/:id", bookVisit);
+router.post("/allBookings", getAllBookings);
+router.delete("/removeBooking/:id", cancelBooking);
+router.post("/toFav/:rid", toFav);
+router.post("/allFav", getAllFavorites); 
+router.delete("/removeFav/:rid", removeFav); 
+
+export default router;
